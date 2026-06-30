bf641ae967b75d96801dcbf4f777a37bc5542e58
XluzyHosting <fahtyrrahmann@gmail.com>
2026-06-30 13:56:29 +0700

    Add initial Group module with initialization function

--- Modules/Group.lua
+++ Modules/Group.lua
@@ -1,1 +1,12 @@
+--//====================================================
+--// StudioLiteTools
+--// Group.lua
+--//====================================================
 
+local Group = {}
+
+function Group.Init(Data)
+	print("[StudioLiteTools] Group Loaded")
+end
+
+return Group
