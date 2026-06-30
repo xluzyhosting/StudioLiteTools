ae9e8ea13148e4660bb990d74030d0d1e5a313c6
XluzyHosting <fahtyrrahmann@gmail.com>
2026-06-30 13:57:17 +0700

    Implement Toolbox initialization function
    Added initialization function and print statement.

--- Modules/Toolbox.lua
+++ Modules/Toolbox.lua
@@ -1,1 +1,12 @@
+--//====================================================
+--// StudioLiteTools
+--// Toolbox.lua
+--//====================================================
 
+local Toolbox = {}
+
+function Toolbox.Init(Data)
+	print("[StudioLiteTools] Toolbox Loaded")
+end
+
+return Toolbox
