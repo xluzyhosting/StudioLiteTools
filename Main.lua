234cc49078a1c16386b4d56be475ad73b2bfabab
XluzyHosting <fahtyrrahmann@gmail.com>
2026-06-30 13:54:24 +0700

    Implement initialization for StudioLiteTools
    Added initialization code for StudioLiteTools.

--- Main.lua
+++ Main.lua
@@ -1,1 +1,16 @@
+--//====================================================
+--// StudioLiteTools
+--// Main.lua
+--// Repository : xluzyhosting/StudioLiteTools
+--//====================================================
 
+local BASE_URL = "https://raw.githubusercontent.com/xluzyhosting/StudioLiteTools/main"
+
+local Success, Result = pcall(function()
+	return loadstring(game:HttpGet(BASE_URL .. "/Init.lua"))()
+end)
+
+if not Success then
+	warn("[StudioLiteTools] Initialization Failed")
+	warn(Result)
+end
