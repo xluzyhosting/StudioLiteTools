f4832423981f87321e70d816de989fd25e173d3a
XluzyHosting <fahtyrrahmann@gmail.com>
2026-06-30 13:56:44 +0700

    Add initial Network module with initialization function

--- Modules/Network.lua
+++ Modules/Network.lua
@@ -1,1 +1,12 @@
+--//====================================================
+--// StudioLiteTools
+--// Network.lua
+--//====================================================
 
+local Network = {}
+
+function Network.Init()
+	print("[StudioLiteTools] Network Loaded")
+end
+
+return Network
