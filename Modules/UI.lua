c757d5b6e207f3594b1fb874f1167d69253de6e1
XluzyHosting <fahtyrrahmann@gmail.com>
2026-06-30 13:57:02 +0700

    Update UI.lua

--- Modules/UI.lua
+++ Modules/UI.lua
@@ -1,1 +1,12 @@
+--//====================================================
+--// StudioLiteTools
+--// UI.lua
+--//====================================================
 
+local UI = {}
+
+function UI.Init(Data)
+	print("[StudioLiteTools] UI Loaded")
+end
+
+return UI
